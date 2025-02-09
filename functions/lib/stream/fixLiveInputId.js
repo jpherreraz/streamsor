8653d6675d08750cb2f6d18ce4939ef81a405e8b
Juan Herrera <juanpablozaid@gmail.com>
2025-02-09 12:46:01 -0600

    refactor: remove hardcoded credentials and update gitignore 🔒

--- functions/lib/stream/fixLiveInputId.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// This file can be deleted as we no longer need to fix live input IDs 
-//# sourceMappingURL=fixLiveInputId.js.map
--- scripts/cleanup-streams.ts
+++ scripts/cleanup-streams.ts
@@ -1,26 +1,14 @@
-import { cert, initializeApp } from 'firebase-admin/app';
+import * as admin from 'firebase-admin';
 import { getDatabase } from 'firebase-admin/database';
+import * as path from 'path';
 
-const serviceAccount = {
-  "type": "service_account",
-  "project_id": "streamsor-6fb0e",
-  "private_key_id": "e0c0c9c9c9c9c9c9c9c9c9c9c9c9c9c9c9c9c9c9",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC9QZ6B7xX6+Yjq\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-xxxxx@streamsor-6fb0e.iam.gserviceaccount.com",
-  "client_id": "xxxxxxxxxxxxxxxxxxxxx",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-xxxxx%40streamsor-6fb0e.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-};
-
-const app = initializeApp({
-  credential: cert(serviceAccount),
+// Initialize Firebase Admin with credentials
+admin.initializeApp({
+  credential: admin.credential.cert(path.join(__dirname, '../functions/src/serviceAccount.json')),
   databaseURL: 'https://streamsor-6fb0e-default-rtdb.firebaseio.com'
 });
 
-const database = getDatabase(app);
+const database = getDatabase();
 
 async function cleanupStreams() {
   try {
